ffa13b9f25c0a9115d9dcabf63bad4931a745147
Risa Chan <142101060+glyzaarcibal@users.noreply.github.com>
2026-02-28 17:28:46 +0800

    Update axios.instance.js

--- frontend/src/utils/axios.instance.js
+++ frontend/src/utils/axios.instance.js
@@ -3,9 +3,9 @@ import { store } from "../store/store";
 import { clearUser } from "../store/slices/authSlice";
 
 const axiosInstance = axios.create({
-  //baseURL: "http://localhost:5000/api",
-  baseURL: import.meta.env.VITE_API_URL || "https://vera-7nnk.vercel.app/api",
-   //: "https://vera-7nnk.vercel.app/api",
+ 
+  baseURL: import.meta.env.VITE_API_URL ,
+  
   timeout: 30000,
   withCredentials: true,
   headers: {
